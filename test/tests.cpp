9111c9a18811bf80d9757fe4ca08ea5936156c41
dselivanovskaya <selivanovskaya.dasha@mail.ru>
2020-01-21 00:12:38 +0300

    Selivanovskaya Dasha b91

--- test/tests.cpp
+++ test/tests.cpp
@@ -4,64 +4,67 @@
 #include "Circle.h"
 #include "DateTime.h"
 #include <string>
-	
-TEST(circle,circle_get_area1)
+
+TEST(circle, circle_get_area1)
 {
-    Circle c(0.0);
-    EXPECT_DOUBLE_EQ(0.0,c.getArea());
+	Circle c(0.0);
+	EXPECT_DOUBLE_EQ(0.0, c.getArea());
 }
-TEST(circle,circle_get_ference1)
+TEST(circle, circle_get_ference1)
 {
-    Circle c(0.0);
-    EXPECT_DOUBLE_EQ(0.0,c.getFerence());
+	Circle c(0.0);
+	EXPECT_DOUBLE_EQ(0.0, c.getFerence());
 }
-TEST(circle,circle_get_area2)
+TEST(circle, circle_get_area2)
 {
-    Circle c(10.0);
-    EXPECT_DOUBLE_EQ(314.0,c.getArea());
+	Circle c(10.0);
+	EXPECT_DOUBLE_EQ(314.0, c.getArea());
+	EXPECT_DOUBLE_EQ(314.15000000000003, c.getArea());
 }
-TEST(circle,circle_get_ference2)
+TEST(circle, circle_get_ference2)
 {
-    Circle c(10.0);
-    EXPECT_DOUBLE_EQ(62.8,c.getFerence());
+	Circle c(10.0);
+	EXPECT_DOUBLE_EQ(62.8, c.getFerence());
+	EXPECT_DOUBLE_EQ(62.830000000000005, c.getFerence());
 }
-TEST(date,datetime_gettomorrow1)
+TEST(date, datetime_gettomorrow1)
 {
-    DateTime dt(31,10,2018);
-    std::string expect="01 november 2018, thursday";
-    EXPECT_EQ(expect,dt.getTomorrow());
+	DateTime dt(31, 10, 2018);
+	std::string expect = "01 november 2018, thursday";
+	EXPECT_EQ(expect, dt.getTomorrow());
 }
-TEST(date,datetime_gettomorrow2)
+TEST(date, datetime_gettomorrow2)
 {
-    DateTime dt(1,11,2018);
-    std::string expect="02 november 2018, friday";
-    EXPECT_EQ(expect,dt.getTomorrow());
+	DateTime dt(1, 11, 2018);
+	std::string expect = "02 november 2018, friday";
+	EXPECT_EQ(expect, dt.getTomorrow());
 }
-TEST(date,datetime_gettoday)
+TEST(date, datetime_gettoday)
 {
-    DateTime dt(1,11,2018);
-    std::string expect="01 november 2018, thursday";
-    EXPECT_EQ(expect,dt.getToday());
+	DateTime dt(1, 11, 2018);
+	std::string expect = "01 november 2018, thursday";
+	EXPECT_EQ(expect, dt.getToday());
 }
-TEST(date,datetime_getdiff1)
+TEST(date, datetime_getdiff1)
 {
-    DateTime dt1(20,11,2018);
-    DateTime dt2(20,11,2018);
-    int diff=0;
-    EXPECT_EQ(diff,dt1.getDifference(dt2));
+	DateTime dt1(20, 11, 2018);
+	DateTime dt2(20, 11, 2018);
+	int diff = 0;
+	EXPECT_EQ(diff, dt1.getDifference(dt2));
 }
-TEST(date,datetime_getdiff2)
+TEST(date, datetime_getdiff2)
 {
-    DateTime dt1(20,11,2018);
-    DateTime dt2(21,11,2018);
-    int diff=1;
-    EXPECT_EQ(diff,dt2.getDifference(dt1));
+	DateTime dt1(20, 11, 2018);
+	DateTime dt2(21, 11, 2018);
+	int diff = 1;
+	EXPECT_EQ(diff, dt2.getDifference(dt1));
 }
-TEST(calc,delta)
-{    
-    EXPECT_DOUBLE_EQ(0.159,calcDelta());
+TEST(calc, delta)
+{
+	EXPECT_DOUBLE_EQ(0.159, calcDelta());
+	EXPECT_DOUBLE_EQ(0.1591596370562911, calcDelta());
 }
-TEST(calc,pool_cost)
-{    
-    EXPECT_DOUBLE_EQ(72254.5,calcCost());
+TEST(calc, pool_cost)
+{
+	EXPECT_DOUBLE_EQ(72254.5, calcCost());
 }
